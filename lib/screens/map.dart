9b8c8b4ba2c9a8ab4cbe136d98a6c32736637767
GhadeerHamed <eng.gh.a.h93@gmail.com>
2026-03-02 03:22:17 +0200

    Add Google Maps integration and update Place model for location handling

---
+++ lib/screens/map.dart
@@ -0,0 +1,58 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({
+    super.key,
+    this.location = const PlaceLocation(
+      latitude: 37.422,
+      longitude: -122.084,
+      address: '1600 Amphitheatre Parkway, Mountain View, CA',
+    ),
+    this.isSelecting = true,
+  });
+
+  final PlaceLocation location;
+  final bool isSelecting;
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: widget.isSelecting
+            ? const Text('Pick Your Location')
+            : const Text('Your Location'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                Navigator.of(context).pop(widget.location);
+              },
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(widget.location.latitude, widget.location.longitude),
+          zoom: 16,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId('m1'),
+            position: LatLng(
+              widget.location.latitude,
+              widget.location.longitude,
+            ),
+          ),
+        },
+      ),
+    );
+  }
+}
