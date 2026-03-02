688e2e924a6196f1c6453c8f9b3f3ade6aaa4ace
GhadeerHamed <eng.gh.a.h93@gmail.com>
2026-03-02 16:53:07 +0200

    Refactor Place model to allow optional ID and implement database handling for user places

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,14 +1,79 @@
 import 'dart:io';
 
 import 'package:favorite_places/models/place.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:flutter_riverpod/legacy.dart';
+
+Future<Database> _getDatabase() async {
+  final dbpath = await sql.getDatabasesPath();
+  return await sql.openDatabase(
+    path.join(dbpath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE user_places('
+        'id TEXT PRIMARY KEY,'
+        'title TEXT,'
+        'image TEXT,'
+        'loc_lat REAL,'
+        'loc_lng REAL,'
+        'address TEXT'
+        ')',
+      );
+    },
+    version: 1,
+  );
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final place = Place(title: title, image: image, location: location);
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+
+    final places = data.map(
+      (item) {
+        return Place(
+          id: item['id'] as String,
+          title: item['title'] as String,
+          image: File(item['image'] as String),
+          location: PlaceLocation(
+            latitude: item['loc_lat'] as double,
+            longitude: item['loc_lng'] as double,
+            address: item['address'] as String,
+          ),
+        );
+      },
+    );
+
+    state = places.toList();
+  }
+
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final savedImage = await image.copy('${appDir.path}/$fileName');
+
+    final place = Place(title: title, image: savedImage, location: location);
+
+    final db = await _getDatabase();
+    await db.insert(
+      'user_places',
+      {
+        'id': place.id,
+        'title': place.title,
+        'image': place.image.path,
+        'loc_lat': place.location.latitude,
+        'loc_lng': place.location.longitude,
+        'address': place.location.address,
+      },
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+
     state = [...state, place];
   }
 }
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -4,11 +4,24 @@ import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PlacesScreen> createState() => _PlacesScreenState();
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final places = ref.watch(userPlacesProvider);
 
     return Scaffold(
@@ -27,7 +40,18 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(places: places),
+        child: FutureBuilder<void>(
+          future: _placesFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return const Center(child: Text('Error loading places.'));
+            }
+            return PlacesList(places: places);
+          },
+        ),
       ),
     );
   }
